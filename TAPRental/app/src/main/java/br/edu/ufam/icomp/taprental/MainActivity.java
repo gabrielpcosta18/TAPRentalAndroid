386bca44867a1f4e5624535105d442c41f6fb824
Gabriel Costa <gabrielp.costa@outlook.com>
2017-01-31 12:20:19 -0400

    Funcionalidade de Alugar disponível

--- TAPRental/app/src/main/java/br/edu/ufam/icomp/taprental/MainActivity.java
+++ TAPRental/app/src/main/java/br/edu/ufam/icomp/taprental/MainActivity.java
@@ -34,7 +34,6 @@ import br.edu.ufam.icomp.taprental.ui.RentRegisterActivity;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private static final int REFRESH_LIST = 1;
-
     private ListView rentalList;
 
     @Override
@@ -49,14 +48,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabRentListAdd);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, RentRegisterActivity.class);
+                startActivityForResult(intent, REFRESH_LIST);
             }
-        });*/
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
